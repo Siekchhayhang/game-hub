18ec6dfd1a1bcb25bd0bd2af902989ec4098fa81
Siek Chhaihang <chhayhangs@gmail.com>
2024-09-07 12:56:00 +0700

    "import Chakra UI"

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { render } from 'preact'
+import { ChakraProvider } from '@chakra-ui/react'
 import { App } from './app.tsx'
 import './index.css'
 
-render(<App />, document.getElementById('app')!)
+render(
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
+    , document.getElementById('app')!)
